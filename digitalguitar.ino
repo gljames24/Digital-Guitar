6bafa3a213dfcf6d2aa2e804b1df372944658144
gljames24 <gljames24@gmail.com>
2022-11-15 14:18:25 -0700

    Update digitalguitar.ino

--- digitalguitar.ino
+++ digitalguitar.ino
@@ -8,287 +8,81 @@
 #include <Keypad.h>
 #include <LibPrintf.h>
 
-
-// Choose your prefered pixmap
-//#include "heart24.h"
-//#include "yellowsmiley24.h"
-//#include "bluesmiley24.h"
-#include "smileytongue24.h"
-#ifndef PSTR
- #define PSTR // Make Arduino Due happy
-#endif
-
 // Allow temporaly dithering, does not work with ESP32 right now
-#ifndef ESP32
+
 #define delay FastLED.delay
-#endif
 
-#if defined(ESP32) or defined(ESP8266)
-#define PIN 12 // GPIO5 = D1
-#else
+
 #define PIN 13
-#endif
-
-
-#define P16BY16X4
-//#define P32BY8X3
-#if defined(P16BY16X4) || defined(P32BY8X3)
-#define BM32
-#endif
-
-#ifdef BM32
-#include "google32.h"
-// Anything with black does not look so good with the naked eye (better on pictures)
-//#include "linux32.h"
-#endif
-
-#include "guitar24.h"
 
 // Max is 255, 32 is a conservative value to not overload
 // a USB power supply (500mA) for 12x12 pixels.
 #define BRIGHTNESS 60
 
 
-#define mw 6
-#define mh 16
-#define NUMMATRIX (mw*mh)
+#define ROW 6
+#define COL 16
+#define NUMMATRIX (ROW*COL)
 CRGB leds[NUMMATRIX];
 // Define matrix width and height.
-FastLED_NeoMatrix *matrix = new FastLED_NeoMatrix(leds, mw, mh, 
+FastLED_NeoMatrix *matrix = new FastLED_NeoMatrix(leds, ROW, COL, 
   NEO_MATRIX_BOTTOM     + NEO_MATRIX_LEFT +
     NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG);
 
-
-// This could also be defined as matrix->color(255,0,0) but those defines
-// are meant to work for adafruit_gfx backends that are lacking color()
-#define LED_BLACK		0
-
-#define LED_RED_VERYLOW 	(3 <<  11)
-#define LED_RED_LOW 		(7 <<  11)
-#define LED_RED_MEDIUM 		(15 << 11)
-#define LED_RED_HIGH 		(31 << 11)
-
-#define LED_GREEN_VERYLOW	(1 <<  5)   
-#define LED_GREEN_LOW 		(15 << 5)  
-#define LED_GREEN_MEDIUM 	(31 << 5)  
-#define LED_GREEN_HIGH 		(63 << 5)  
-
-#define LED_BLUE_VERYLOW	3
-#define LED_BLUE_LOW 		7
-#define LED_BLUE_MEDIUM 	15
-#define LED_BLUE_HIGH 		31
-
-#define LED_ORANGE_VERYLOW	(LED_RED_VERYLOW + LED_GREEN_VERYLOW)
-#define LED_ORANGE_LOW		(LED_RED_LOW     + LED_GREEN_LOW)
-#define LED_ORANGE_MEDIUM	(LED_RED_MEDIUM  + LED_GREEN_MEDIUM)
-#define LED_ORANGE_HIGH		(LED_RED_HIGH    + LED_GREEN_HIGH)
-
-#define LED_PURPLE_VERYLOW	(LED_RED_VERYLOW + LED_BLUE_VERYLOW)
-#define LED_PURPLE_LOW		(LED_RED_LOW     + LED_BLUE_LOW)
-#define LED_PURPLE_MEDIUM	(LED_RED_MEDIUM  + LED_BLUE_MEDIUM)
-#define LED_PURPLE_HIGH		(LED_RED_HIGH    + LED_BLUE_HIGH)
-
-#define LED_CYAN_VERYLOW	(LED_GREEN_VERYLOW + LED_BLUE_VERYLOW)
-#define LED_CYAN_LOW		(LED_GREEN_LOW     + LED_BLUE_LOW)
-#define LED_CYAN_MEDIUM		(LED_GREEN_MEDIUM  + LED_BLUE_MEDIUM)
-#define LED_CYAN_HIGH		(LED_GREEN_HIGH    + LED_BLUE_HIGH)
-
-#define LED_WHITE_VERYLOW	(LED_RED_VERYLOW + LED_GREEN_VERYLOW + LED_BLUE_VERYLOW)
-#define LED_WHITE_LOW		(LED_RED_LOW     + LED_GREEN_LOW     + LED_BLUE_LOW)
-#define LED_WHITE_MEDIUM	(LED_RED_MEDIUM  + LED_GREEN_MEDIUM  + LED_BLUE_MEDIUM)
-#define LED_WHITE_HIGH		(LED_RED_HIGH    + LED_GREEN_HIGH    + LED_BLUE_HIGH)
-
 // Color definitions
-#define WHITE      0xFFFF
-#define MAGENTA    0xE012
-#define ROSE       0xF009
-#define RED        0xF800
-#define ORANGE     0xEBA0
-#define YELLOW     0xD680
-#define CHARTREUSE 0x7760
-#define GREEN      0x07E0
-#define MINT       0x07A9 
-#define CYAN       0x0732
-#define AZURE      0x0456
-#define BLUE       0x0018
-#define VIOLET     0x8816
-#define BLACK      0x0000
+#define WHITE       0xAAAA
+#define RED         0xF800
+#define ORANGE      0xDB80
+#define YELLOW      0xAD80
+#define CHARTREUSE  0x6EE0
+#define GREEN       0x07E0
+#define VERDIGRIS   0x06ED
+#define CYAN        0x0595
+#define AZURE       0x039B
+#define BLUE        0x001F
+#define PURPLE      0x681B
+#define MAGENTA     0xA815
+#define ROSE        0xD80D
+#define BLACK       0x0000
 
-static const uint8_t PROGMEM
-    mono_bmp[][8] =
-    {
-	{   // 0: checkered 1
-	    B10101010,
-	    B01010101,
-	    B10101010,
-	    B01010101,
-	    B10101010,
-	    B01010101,
-	    B10101010,
-	    B01010101,
-			},
+const uint16_t COLORS [] = {RED, ORANGE, YELLOW, CHARTREUSE, GREEN, VERDIGRIS, CYAN, AZURE, BLUE, PURPLE, MAGENTA, ROSE};
 
-	{   // 1: checkered 2
-	    B01010101,
-	    B10101010,
-	    B01010101,
-	    B10101010,
-	    B01010101,
-	    B10101010,
-	    B01010101,
-	    B10101010,
-			},
+enum Note{A,Bb,B,C,Db,D,Eb,E,F,Gb,G,Ab};
 
-	{   // 2: smiley
-	    B00111100,
-	    B01000010,
-	    B10100101,
-	    B10000001,
-	    B10100101,
-	    B10011001,
-	    B01000010,
-	    B00111100 },
+struct FretKey{
+  uint16_t color;
+  Note note;
+  uint8_t row;
+  uint8_t col;
+  uint8_t octive;
 
-	{   // 3: neutral
-	    B00111100,
-	    B01000010,
-	    B10100101,
-	    B10000001,
-	    B10111101,
-	    B10000001,
-	    B01000010,
-	    B00111100 },
+  FretKey (){
+    color = 0;
+    note = 0;
+    row = 0;
+    col = 0;
+    octive = 0;     
+  }
 
-	{   // 4; frowny
-	    B00111100,
-	    B01000010,
-	    B10100101,
-	    B10000001,
-	    B10011001,
-	    B10100101,
-	    B01000010,
-	    B00111100 },
-    };
+  FretKey (uint16_t clr, Note n, uint8_t r, uint8_t c, uint8_t o){
+    color = clr;
+    note = n;
+    row = r;
+    col = c;
+    octive = o;    
+  }
 
-static const uint16_t PROGMEM
-    // These bitmaps were written for a backend that only supported
-    // 4 bits per color with Blue/Green/Red ordering while neomatrix
-    // uses native 565 color mapping as RGB.  
-    // I'm leaving the arrays as is because it's easier to read
-    // which color is what when separated on a 4bit boundary
-    // The demo code will modify the arrays at runtime to be compatible
-    // with the neomatrix color ordering and bit depth.
-    RGB_bmp[][64] = {
-      // 00: blue, blue/red, red, red/green, green, green/blue, blue, white
-      {	0x100, 0x200, 0x300, 0x400, 0x600, 0x800, 0xA00, 0xF00, 
-	0x101, 0x202, 0x303, 0x404, 0x606, 0x808, 0xA0A, 0xF0F, 
-      	0x001, 0x002, 0x003, 0x004, 0x006, 0x008, 0x00A, 0x00F, 
-	0x011, 0x022, 0x033, 0x044, 0x066, 0x088, 0x0AA, 0x0FF, 
-	0x010, 0x020, 0x030, 0x040, 0x060, 0x080, 0x0A0, 0x0F0, 
-	0x110, 0x220, 0x330, 0x440, 0x660, 0x880, 0xAA0, 0xFF0, 
-	0x100, 0x200, 0x300, 0x400, 0x600, 0x800, 0xA00, 0xF00, 
-	0x111, 0x222, 0x333, 0x444, 0x666, 0x888, 0xAAA, 0xFFF, },
+  void setKeyColor(){
+     matrix->drawPixel(row,col,color);
+  }
 
-      // 01: grey to white
-      {	0x111, 0x222, 0x333, 0x555, 0x777, 0x999, 0xAAA, 0xFFF, 
-	0x222, 0x222, 0x333, 0x555, 0x777, 0x999, 0xAAA, 0xFFF, 
-	0x333, 0x333, 0x333, 0x555, 0x777, 0x999, 0xAAA, 0xFFF, 
-	0x555, 0x555, 0x555, 0x555, 0x777, 0x999, 0xAAA, 0xFFF, 
-	0x777, 0x777, 0x777, 0x777, 0x777, 0x999, 0xAAA, 0xFFF, 
-	0x999, 0x999, 0x999, 0x999, 0x999, 0x999, 0xAAA, 0xFFF, 
-	0xAAA, 0xAAA, 0xAAA, 0xAAA, 0xAAA, 0xAAA, 0xAAA, 0xFFF, 
-	0xFFF, 0xFFF, 0xFFF, 0xFFF, 0xFFF, 0xFFF, 0xFFF, 0xFFF, },
+  void setKeyWhite(){
+     matrix->drawPixel(row,col,WHITE);
+  }
 
-      // 02: low red to high red
-      {	0x001, 0x002, 0x003, 0x005, 0x007, 0x009, 0x00A, 0x00F, 
-	0x002, 0x002, 0x003, 0x005, 0x007, 0x009, 0x00A, 0x00F, 
-	0x003, 0x003, 0x003, 0x005, 0x007, 0x009, 0x00A, 0x00F, 
-	0x005, 0x005, 0x005, 0x005, 0x007, 0x009, 0x00A, 0x00F, 
-	0x007, 0x007, 0x007, 0x007, 0x007, 0x009, 0x00A, 0x00F, 
-	0x009, 0x009, 0x009, 0x009, 0x009, 0x009, 0x00A, 0x00F, 
-	0x00A, 0x00A, 0x00A, 0x00A, 0x00A, 0x00A, 0x00A, 0x00F, 
-	0x00F, 0x00F, 0x00F, 0x00F, 0x00F, 0x00F, 0x00F, 0x00F, },
-
-      // 03: low green to high green
-      {	0x010, 0x020, 0x030, 0x050, 0x070, 0x090, 0x0A0, 0x0F0, 
-	0x020, 0x020, 0x030, 0x050, 0x070, 0x090, 0x0A0, 0x0F0, 
-	0x030, 0x030, 0x030, 0x050, 0x070, 0x090, 0x0A0, 0x0F0, 
-	0x050, 0x050, 0x050, 0x050, 0x070, 0x090, 0x0A0, 0x0F0, 
-	0x070, 0x070, 0x070, 0x070, 0x070, 0x090, 0x0A0, 0x0F0, 
-	0x090, 0x090, 0x090, 0x090, 0x090, 0x090, 0x0A0, 0x0F0, 
-	0x0A0, 0x0A0, 0x0A0, 0x0A0, 0x0A0, 0x0A0, 0x0A0, 0x0F0, 
-	0x0F0, 0x0F0, 0x0F0, 0x0F0, 0x0F0, 0x0F0, 0x0F0, 0x0F0, },
-
-      // 04: low blue to high blue
-      {	0x100, 0x200, 0x300, 0x500, 0x700, 0x900, 0xA00, 0xF00, 
-	0x200, 0x200, 0x300, 0x500, 0x700, 0x900, 0xA00, 0xF00, 
-	0x300, 0x300, 0x300, 0x500, 0x700, 0x900, 0xA00, 0xF00, 
-	0x500, 0x500, 0x500, 0x500, 0x700, 0x900, 0xA00, 0xF00, 
-	0x700, 0x700, 0x700, 0x700, 0x700, 0x900, 0xA00, 0xF00, 
-	0x900, 0x900, 0x900, 0x900, 0x900, 0x900, 0xA00, 0xF00, 
-	0xA00, 0xA00, 0xA00, 0xA00, 0xA00, 0xA00, 0xA00, 0xF00, 
-	0xF00, 0xF00, 0xF00, 0xF00, 0xF00, 0xF00, 0xF00, 0xF00, },
-
-      // 05: 1 black, 2R, 2O, 2G, 1B with 4 blue lines rising right
-      {	0x000, 0x200, 0x000, 0x400, 0x000, 0x800, 0x000, 0xF00, 
-      	0x000, 0x201, 0x002, 0x403, 0x004, 0x805, 0x006, 0xF07, 
-	0x008, 0x209, 0x00A, 0x40B, 0x00C, 0x80D, 0x00E, 0xF0F, 
-	0x000, 0x211, 0x022, 0x433, 0x044, 0x855, 0x066, 0xF77, 
-	0x088, 0x299, 0x0AA, 0x4BB, 0x0CC, 0x8DD, 0x0EE, 0xFFF, 
-	0x000, 0x210, 0x020, 0x430, 0x040, 0x850, 0x060, 0xF70, 
-	0x080, 0x290, 0x0A0, 0x4B0, 0x0C0, 0x8D0, 0x0E0, 0xFF0,
-	0x000, 0x200, 0x000, 0x500, 0x000, 0x800, 0x000, 0xF00, },
-
-      // 06: 4 lines of increasing red and then green
-      { 0x000, 0x000, 0x001, 0x001, 0x002, 0x002, 0x003, 0x003, 
-	0x004, 0x004, 0x005, 0x005, 0x006, 0x006, 0x007, 0x007, 
-	0x008, 0x008, 0x009, 0x009, 0x00A, 0x00A, 0x00B, 0x00B, 
-	0x00C, 0x00C, 0x00D, 0x00D, 0x00E, 0x00E, 0x00F, 0x00F, 
-	0x000, 0x000, 0x010, 0x010, 0x020, 0x020, 0x030, 0x030, 
-	0x040, 0x040, 0x050, 0x050, 0x060, 0x060, 0x070, 0x070, 
-	0x080, 0x080, 0x090, 0x090, 0x0A0, 0x0A0, 0x0B0, 0x0B0, 
-	0x0C0, 0x0C0, 0x0D0, 0x0D0, 0x0E0, 0x0E0, 0x0F0, 0x0F0, },
-
-      // 07: 4 lines of increasing red and then blue
-      { 0x000, 0x000, 0x001, 0x001, 0x002, 0x002, 0x003, 0x003, 
-	0x004, 0x004, 0x005, 0x005, 0x006, 0x006, 0x007, 0x007, 
-	0x008, 0x008, 0x009, 0x009, 0x00A, 0x00A, 0x00B, 0x00B, 
-	0x00C, 0x00C, 0x00D, 0x00D, 0x00E, 0x00E, 0x00F, 0x00F, 
-	0x000, 0x000, 0x100, 0x100, 0x200, 0x200, 0x300, 0x300, 
-	0x400, 0x400, 0x500, 0x500, 0x600, 0x600, 0x700, 0x700, 
-	0x800, 0x800, 0x900, 0x900, 0xA00, 0xA00, 0xB00, 0xB00, 
-	0xC00, 0xC00, 0xD00, 0xD00, 0xE00, 0xE00, 0xF00, 0xF00, },
-
-      // 08: criss cross of green and red with diagonal blue.
-      {	0xF00, 0x001, 0x003, 0x005, 0x007, 0x00A, 0x00F, 0x000, 
-	0x020, 0xF21, 0x023, 0x025, 0x027, 0x02A, 0x02F, 0x020, 
-	0x040, 0x041, 0xF43, 0x045, 0x047, 0x04A, 0x04F, 0x040, 
-	0x060, 0x061, 0x063, 0xF65, 0x067, 0x06A, 0x06F, 0x060, 
-	0x080, 0x081, 0x083, 0x085, 0xF87, 0x08A, 0x08F, 0x080, 
-	0x0A0, 0x0A1, 0x0A3, 0x0A5, 0x0A7, 0xFAA, 0x0AF, 0x0A0, 
-	0x0F0, 0x0F1, 0x0F3, 0x0F5, 0x0F7, 0x0FA, 0xFFF, 0x0F0, 
-	0x000, 0x001, 0x003, 0x005, 0x007, 0x00A, 0x00F, 0xF00, },
-
-      // 09: 2 lines of green, 2 red, 2 orange, 2 green
-      { 0x0F0, 0x0F0, 0x0FF, 0x0FF, 0x00F, 0x00F, 0x0F0, 0x0F0, 
-	0x0F0, 0x0F0, 0x0FF, 0x0FF, 0x00F, 0x00F, 0x0F0, 0x0F0, 
-	0x0F0, 0x0F0, 0x0FF, 0x0FF, 0x00F, 0x00F, 0x0F0, 0x0F0, 
-	0x0F0, 0x0F0, 0x0FF, 0x0FF, 0x00F, 0x00F, 0x0F0, 0x0F0, 
-	0x0F0, 0x0F0, 0x0FF, 0x0FF, 0x00F, 0x00F, 0x0F0, 0x0F0, 
-	0x0F0, 0x0F0, 0x0FF, 0x0FF, 0x00F, 0x00F, 0x0F0, 0x0F0, 
-	0x0F0, 0x0F0, 0x0FF, 0x0FF, 0x00F, 0x00F, 0x0F0, 0x0F0, 
-	0x0F0, 0x0F0, 0x0FF, 0x0FF, 0x00F, 0x00F, 0x0F0, 0x0F0, },
-
-      // 10: multicolor smiley face
-      { 0x000, 0x000, 0x00F, 0x00F, 0x00F, 0x00F, 0x000, 0x000, 
-	0x000, 0x00F, 0x000, 0x000, 0x000, 0x000, 0x00F, 0x000, 
-	0x00F, 0x000, 0xF00, 0x000, 0x000, 0xF00, 0x000, 0x00F, 
-	0x00F, 0x000, 0x000, 0x000, 0x000, 0x000, 0x000, 0x00F, 
-	0x00F, 0x000, 0x0F0, 0x000, 0x000, 0x0F0, 0x000, 0x00F, 
-	0x00F, 0x000, 0x000, 0x0F4, 0x0F3, 0x000, 0x000, 0x00F, 
-	0x000, 0x00F, 0x000, 0x000, 0x000, 0x000, 0x00F, 0x000, 
-	0x000, 0x000, 0x00F, 0x00F, 0x00F, 0x00F, 0x000, 0x000, },
 };
+
+FretKey guitar[ROW][COL+1];
 
 // Convert a BGR 4/4/4 bitmap to RGB 5/6/5 used by Adafruit_GFX
 void fixdrawRGBBitmap(int16_t x, int16_t y, const uint16_t *bitmap, int16_t w, int16_t h) {
@@ -324,37 +118,15 @@ void fixdrawRGBBitmap(int16_t x, int16_t y, const uint16_t *bitmap, int16_t w, i
     matrix->drawRGBBitmap(x, y, RGB_bmp_fixed, w, h);
 }
 
-// In a case of a tile of neomatrices, this test is helpful to make sure that the
-// pixels are all in sequence (to check your wiring order and the tile options you
-// gave to the constructor).
-void count_pixels() {
-    matrix->clear();
-    for (uint16_t i=0; i<mh; i++) {
-	for (uint16_t j=0; j<mw; j++) {
-	    matrix->drawPixel(j, i, i%3==0?(uint16_t)LED_BLUE_HIGH:i%3==1?(uint16_t)LED_RED_HIGH:(uint16_t)LED_GREEN_HIGH);
-	    // depending on the matrix size, it's too slow to display each pixel, so
-	    // make the scan init faster. This will however be too fast on a small matrix.
-	    #ifdef ESP8266
-	    if (!(j%3)) matrix->show();
-	    yield(); // reset watchdog timer
-	    #elif ESP32
-	    delay(1);
-	    matrix->show();
-	    #else 
-	    matrix->show();
-	    #endif
-	}
-    }
-}
 
 //
 void draw_guitar() {
     matrix->clear();
-    uint16_t notes [] = {RED, ORANGE, YELLOW, CHARTREUSE, GREEN, MINT, CYAN, AZURE, BLUE, VIOLET, MAGENTA, ROSE};
+    uint16_t notes [] = {RED, ORANGE, YELLOW, CHARTREUSE, GREEN, VERDIGRIS, CYAN, AZURE, BLUE, PURPLE, MAGENTA, ROSE};
     int offSet [] = {7, 0, 5, 10, 2, 7};
-    for (uint16_t i=0; i<mw; i++) {
+    for (uint16_t i=0; i<ROW; i++) {
       int k = offSet[i];
-  	  for (uint16_t j=0; j<mh; j++) {
+  	  for (uint16_t j=0; j<COL; j++) {
         k++;
         k %= 12;
         matrix->drawPixel(i, j, notes[k]);
@@ -453,71 +225,164 @@ void color_check(){
   }
 }
 
+uint16_t hsv2rgb565(uint8_t hue, uint8_t sat, uint8_t val){
+  uint8_t red = 0;
+  uint8_t green = 0;
+  uint8_t blue = 0;
+
+  uint8_t phase = (uint8_t) hue/42.5;
+  uint8_t vSat = map(sat, 0, 255, val, 0);
+  uint8_t chroma = 255*(val/255)*(sat/255);
+
+  //printf("%d, %d, %d, %d\n", phase, vSat, chroma, hue);
+
+  switch (phase){
+    case 0 : 
+      red = val;
+      green = (hue*6);
+      blue = vSat;
+      break;
+    case 1 :
+      red = chroma-(hue*6)%chroma;
+      green = val;
+      blue = vSat;
+      break;
+    case 2 :
+      red = vSat;
+      green = val;
+      blue = (hue*6)%chroma;
+      break;
+    case 3 :
+      red = vSat;
+      green = chroma-(hue*6)%chroma;
+      blue = val;            
+      break;
+    case 4 :
+      red = (hue*6)%chroma;
+      green = vSat;
+      blue = val;          
+      break;      
+    case 5 :
+      red = val;
+      green = vSat;
+      blue = chroma-(hue*6)%chroma;
+      break;          
+    default:
+      red = val;
+      green = vSat;
+      blue = vSat;      
+      break;      
+    }
+    
+    //green *= 0.9;
+    //blue *= 0.65;
+
+    if(chroma!=0){
+      double normVec = chroma/sqrt((uint32_t)red*red+(uint32_t)green*green+(uint32_t)blue*blue);
+      red *= normVec;
+      green *= normVec;
+      blue *= normVec;
+      printf("%d, %d, %d, %f\n", blue, red, green, normVec);
+    }
+   
+
+    
+    
+    return (map(red, 0, 255, 0, 31) << 11) + (map(green, 0, 255, 0, 63) << 5) + map(blue, 0, 255, 0, 31);
+}
+
+int * getHandShape(){
+  static int handShape[ROW] = {-1};
+  
+  for(uint8_t x=0;x<COL;x++){
+    PORTC &= 0xF0; //clear the four bits we want to increment
+    PORTC |= x;    //set the four bits we want to increment
+    for(uint8_t y=0;y<ROW;y++){
+      if(handShape[y] ==-1 && digitalRead(rowPins[y])){
+        handShape[y] = x;
+      }      
+    }
+  }
+  return handShape;
+}
+
+uint8_t rowPins[] = {21,8,9,10,11,12};
+
+void setup() {
+
+  //Setup keyboard
+  //Keyboard col port output
+  DDRC |= 0x0F;
+  PORTC = B00000001;
+  //Keyboard row input
+  for(uint8_t y=0;y<ROW;y++){
+    pinMode(rowPins[y],INPUT);
+  }
+
+  // Time for serial port to work?
+  delay(1000);
+  Serial.begin(115200);
+  printf("Init on pin: %d\nMatrix Size: %d\n %d\n %d\n", PIN, ROW, COL, NUMMATRIX);
+
+
+  matrix->begin();
+  matrix->setTextWrap(false);
+  matrix->setBrightness(BRIGHTNESS);
+  Serial.println("If the code crashes here, decrease the brightness or turn off the all white display below");
+  // Test full bright of all LEDs. If brightness is too high
+  // for your current limit (i.e. USB), decrease it.
+  //#define DISABLE_WHITE
+  #ifndef DISABLE_WHITE
+      matrix->fillScreen(WHITE);
+      matrix->show();
+      delay(3000);
+      matrix->clear();
+  #endif
+
+  FretKey guitar[ROW][COL+1];
+
+  
+  const int tuningOffset [] = {7, 0, 5, 10, 2, 7};//EADGBE tuning, would like to make adjustable in the future
+  const int octiveOffset [] = {2,2,3,3,3,4};
+  for (uint8_t i=0; i<ROW; i++) {//Set guitar strings
+    int k = tuningOffset[5-i];//fret and string 0,0 is high E
+    int l = octiveOffset[5-i];
+    for (uint8_t j=0; j<COL+1; j++) {//Set guitar frets
+      guitar[i][j].color = new FretKey (COLORS[k], k, i, j, l);
+      k++;
+      k %= 12;
+      if(k=3){
+        l++;
+      }            
+    }
+  }  
+}
+
 void loop() {
-    // clear the screen after X bitmaps have been displayed and we
-    // loop back to the top left corner
-    // 8x8 => 1, 16x8 => 2, 17x9 => 6
-    static uint8_t pixmap_count = ((mw+7)/8) * ((mh+7)/8);
 
 // You can't use millis to time frame fresh rate because it uses cli() which breaks millis()
 // So I use my stopwatch to count 200 displays and that's good enough
-#if 0
-    // 200 displays in 13 seconds = 15 frames per second for 4096 pixels
-    for (uint8_t i=0; i<100; i++) { 
-	matrix->fillScreen(LED_BLUE_LOW);
-	matrix->show();
-	matrix->fillScreen(LED_RED_LOW);
-	matrix->show();
-    }
-#endif
-
-  draw_guitar();
-  delay(2500);
-
-  draw_rainbow_animation();
-
-  color_check();
-         
-  while(true){
-    
-  }
-}
-
-void setup() {
-    // Time for serial port to work?
-    delay(1000);
-    Serial.begin(115200);
-    Serial.print("Init on pin: ");
-    Serial.println(PIN);
-    Serial.print("Matrix Size: ");
-    Serial.print(mw);
-    Serial.print(" ");
-    Serial.print(mh);
-    Serial.print(" ");
-    Serial.println(NUMMATRIX);
-
-#if defined(P32BY8X3)
-    // Parallel output
-    FastLED.addLeds<WS2811_PORTA,3>(leds, NUMMATRIX/3).setCorrection(TypicalLEDStrip);
-    Serial.print("Setup parrallel WS2811_PORTA: ");
-    Serial.print(NUMMATRIX);
-#else
-    FastLED.addLeds<NEOPIXEL,PIN>(  leds, NUMMATRIX  ).setCorrection(TypicalLEDStrip);
-    Serial.print("Setup serial: ");
-    Serial.println(NUMMATRIX);
-#endif
-
-    matrix->begin();
-    matrix->setTextWrap(false);
-    matrix->setBrightness(BRIGHTNESS);
-    Serial.println("If the code crashes here, decrease the brightness or turn off the all white display below");
-    // Test full bright of all LEDs. If brightness is too high
-    // for your current limit (i.e. USB), decrease it.
-//#define DISABLE_WHITE
-#ifndef DISABLE_WHITE
-    matrix->fillScreen(LED_WHITE_HIGH);
+  #if 0
+      // 200 displays in 13 seconds = 15 frames per second for 4096 pixels
+      for (uint8_t i=0; i<100; i++) { 
+    matrix->fillScreen(BLUE);
     matrix->show();
-    delay(3000);
-    matrix->clear();
-#endif
+    matrix->fillScreen(RED);
+    matrix->show();
+      }
+  #endif
+
+  uint8_t * handShape = getHandShape();
+  for(int i=0;i<ROW;i++){
+   guitar[i][handShape[i]].setKeyWhite();
+  }
+
+
+         
+  // for(int x=0; x<256; x++){
+  //   matrix->fillScreen(hsv2rgb565(x,255,255));
+  //   delay(100);
+  //   matrix->show();
+  // }  
+
 }
